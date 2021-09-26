6de1a430e141cbf8cf038daff8015dfc45b2bde4
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-09-26 17:50:41 -0400

    added exercise 32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution32 {
+    public static void main(String[] args) {
+        //create instance of the guessing game
+        GuessingGame guessingGame = new GuessingGame();
+        //call method to play the guessing game
+        guessingGame.playGuessingGame();
+    }
+}
