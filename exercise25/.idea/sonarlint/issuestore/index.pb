9e82d0c1bda82e9fd82fc001a96f840b84cf3af4
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-10-02 19:07:21 -0400

    added more test cases for exercise 25

--- exercise25/.idea/sonarlint/issuestore/index.pb
+++ exercise25/.idea/sonarlint/issuestore/index.pb
@@ -3,7 +3,7 @@
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 V
 &src/main/java/baseline/Solution25.java,d\e\de910ab7a83b3c9334bcfca066d815e896729cc8
-Y
-)src/test/java/baseline/PasswordsTest.java,3\d\3dd9a7c7fefbd9a2c8d4cab3baebe744a89060bf
 U
 %src/main/java/baseline/Passwords.java,a\f\af335eb9faa09632d898a7a8eb10486f0c223e65
+Y
+)src/test/java/baseline/PasswordsTest.java,3\d\3dd9a7c7fefbd9a2c8d4cab3baebe744a89060bf
--- exercise25/src/test/java/baseline/PasswordsTest.java
+++ exercise25/src/test/java/baseline/PasswordsTest.java
@@ -5,15 +5,20 @@
 
 package baseline;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordsTest {
+    Passwords password;
+    @BeforeEach
+    void createObjects() {
+        password = new Passwords();
+    }
 
     @Test
-    void passwordValidator() {
-        Passwords password = new Passwords();
+    void passwordValidator1() {
         String myPassword = "1337h@xor!";
 
         int actualResult = password.passwordValidator(myPassword);
@@ -22,4 +27,33 @@ class PasswordsTest {
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    void passwordValidator2() {
+        String myPassword = "abc123xyz";
+
+        int actualResult = password.passwordValidator(myPassword);
+        int expectedResult = 3;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void passwordValidator3() {
+        String myPassword = "abcdef";
+
+        int actualResult = password.passwordValidator(myPassword);
+        int expectedResult = 2;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void passwordValidator4() {
+        String myPassword = "12345";
+
+        int actualResult = password.passwordValidator(myPassword);
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
