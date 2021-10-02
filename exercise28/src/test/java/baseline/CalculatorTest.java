dbf6b0e9091a74588f1da78d9d462c5c3ce16db0
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-10-02 19:16:40 -0400

    added more test cases for exercise 28

--- exercise28/src/test/java/baseline/CalculatorTest.java
+++ exercise28/src/test/java/baseline/CalculatorTest.java
@@ -10,11 +10,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    Calculator calculator = new Calculator(new int[]{1, 2, 3, 4, 5});
+    Calculator calculator;
 
     @Test
-    void calculateTotal() {
+    void calculateTotal1() {
+        calculator = new Calculator(new int[]{1, 2, 3, 4, 5});
         String expectedTotal = "The total is 15";
+        String actualTotal = calculator.calculateTotal();
+
+        assertEquals(expectedTotal, actualTotal);
+    }
+
+    @Test
+    void calculateTotal2() {
+        calculator = new Calculator(new int[]{1, 2, 3, 4, 5, 6});
+        String expectedTotal = "The total is 21";
+        String actualTotal = calculator.calculateTotal();
+
+        assertEquals(expectedTotal, actualTotal);
+    }
+
+    @Test
+    void calculateTotal3() {
+        calculator = new Calculator(new int[]{7, 2, 3, 4, 5, 6});
+        String expectedTotal = "The total is 27";
         String actualTotal = calculator.calculateTotal();
 
         assertEquals(expectedTotal, actualTotal);
